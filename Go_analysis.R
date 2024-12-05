9e24c7841945731702cd8b9a510063bb4898130a
L. Mingkai <96586384+LamarckLab@users.noreply.github.com>
2024-12-05 20:41:57 +0800

    Update Go_analysis.R

--- Go_analysis.R
+++ Go_analysis.R
@@ -14,16 +14,6 @@ gene <- gene_data[, 2]
 
 # GO富集分析
 # GO富集的三个部分：CC、BP、MF  这里的P值和Q值都没有限定，因为后续想手动筛选
-ego_ALL <- enrichGO(gene = gene,
-                    OrgDb=org.Hs.eg.db,
-                    keyType = "ENTREZID",
-                    ont = "ALL",
-                    pAdjustMethod = "BH",
-                    minGSSize = 1,
-                    pvalueCutoff = 1,
-                    qvalueCutoff = 1,
-                    readable = TRUE)
-
 ego_CC <- enrichGO(gene = gene,
                    OrgDb=org.Hs.eg.db,
                    keyType = "ENTREZID",
@@ -55,13 +45,11 @@ ego_MF <- enrichGO(gene = gene,
                    readable = TRUE)
 
 # 把得到的四个结果转换成数据框格式
-ego_ALL <- as.data.frame(ego_ALL)
 ego_result_BP <- as.data.frame(ego_BP)
 ego_result_CC <- as.data.frame(ego_CC)
 ego_result_MF <- as.data.frame(ego_MF)
 
 # 把GO富集分析得到的完整列表以及相关信息导出
-write.csv(ego_ALL,file = "C:/Users/Lamarck/Desktop/ego_ALL.csv",row.names = T)
 write.csv(ego_result_BP,file = "C:/Users/Lamarck/Desktop/ego_result_BP.csv",row.names = T)
 write.csv(ego_result_CC,file = "C:/Users/Lamarck/Desktop/ego_result_CC.csv",row.names = T)
 write.csv(ego_result_MF,file = "C:/Users/Lamarck/Desktop/ego_result_MF.csv",row.names = T)
